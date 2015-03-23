201880ce4218077d24aca56e9ef474f68da749f7
葫芦娃 <huwenhua@qiaobutang.com>
2015-03-23 23:09:27 +0800

    update

--- public/js/post.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	var $content = $('#post-content'),
-		$title = $('#post-title'),
-		$titleInput = $('.post-title_input'),
-		$postBtn = $('#post-btn'),
-		article = {},
-		$post = $('#post');
-})();
